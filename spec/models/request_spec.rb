913278be127c352f47f56d77de22a064d6bc18c2
Daniel <daniel.wojnar13@gmail.com>
2020-04-11 18:50:14 +0200

    feat(rubocop and reek): add code smell detectors

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -1,29 +1,31 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Request, type: :model do
   context 'without a phone number' do
     it 'does not save a record' do
-      request = Request.new(phone_number: '', message_content: 'Groceries')
+      request = described_class.new(phone_number: '', message_content: 'Groceries')
 
-      expect { request.save }.not_to change(Request, :count)
+      expect { request.save }.not_to change(described_class, :count)
       expect(request.errors.full_messages).to include("Phone number can't be blank")
     end
   end
 
   context 'without a message content' do
     it 'does not save a record' do
-      request = Request.new(phone_number: '123456789', message_content: '')
+      request = described_class.new(phone_number: '123456789', message_content: '')
 
-      expect { request.save }.not_to change(Request, :count)
+      expect { request.save }.not_to change(described_class, :count)
       expect(request.errors.full_messages).to include("Message content can't be blank")
     end
   end
 
   context 'valid phone number and mesage content' do
     it 'saves a record' do
-      request = Request.new(phone_number: '123456789', message_content: 'Pharmacy')
+      request = described_class.new(phone_number: '123456789', message_content: 'Pharmacy')
 
-      expect { request.save }.to change(Request, :count).from(0).to(1)
+      expect { request.save }.to change(described_class, :count).from(0).to(1)
       request.reload
       expect(request.phone_number).to eq('123456789')
       expect(request.message_content).to eq('Pharmacy')
