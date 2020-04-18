3dd907d0183cbd700ced07af7da16b10a110d1e3
Daniel <daniel.wojnar13@gmail.com>
2020-04-18 18:18:20 +0200

    feat(user): add jwt token generation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def generate_jwt
+    JWT.encode(
+      {
+        id: id,
+        exp: 30.days.from.now.to_i
+      },
+      Rails.application.secrets.secret_key_base
+    )
+  end
 end
+
