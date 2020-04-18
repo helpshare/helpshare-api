ca40c69ac069f96f69501165c84c0aecb2066b58
Daniel <daniel.wojnar13@gmail.com>
2020-04-18 20:34:55 +0200

    improvement(devise): remove devise
    We dont want it for api

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,4 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-  def generate_jwt
-    JWT.encode(
-      {
-        id: id,
-        exp: 30.days.from_now.to_i
-      },
-      Rails.application.secrets.secret_key_base
-    )
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  scope :api do
-    devise_for :users, controllers: { sessions: :sessions }, path_names: { sign_in: :login }
-  end
 end
+
