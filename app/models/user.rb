c2642b4f59a4eaae0862caa11d8697314de2428f
Daniel <daniel.wojnar13@gmail.com>
2020-05-02 18:34:46 +0200

    feat(user): user registration token validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: { case_senstive: false }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
+  validates :registration_token, uniqueness: true, allow_nil: true
 end
